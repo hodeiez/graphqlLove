c8facb0ecd8d78e7da5d45c074534658e52cba5c
hodeiez <hodeinaiz@hotmail.com>
2022-05-16 22:08:00 +0200

    it finds shortestpath from source

--- src/main/java/hodei/naiz/graphqllove/helpers/ShortestPath.java
+++ src/main/java/hodei/naiz/graphqllove/helpers/ShortestPath.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
 
 
 /**
@@ -20,16 +20,35 @@ public class ShortestPath {
     private Graph graph;
     private int[] distance;
     private HashMap<Integer,Set<Integer>> paths;
-
+    private List<Integer> selectedPath;
+    private int selectedDistance;
     public ShortestPath(Graph graph){
         this.graph = graph;
 
         distance= IntStream.range(0, graph.getAdjacentNodes().size()).map(i->i=Integer.MAX_VALUE).toArray();
         distance[0]=0;
-        paths=new HashMap<Integer,Set<Integer>>();
+        paths=new HashMap<>();
+    }
+    public ShortestPath(Graph graph,int target){
+        this.graph = graph;
+        distance= IntStream.range(0, graph.getAdjacentNodes().size()).map(i->i=Integer.MAX_VALUE).toArray();
+        distance[0]=0;
+        paths=new HashMap<>();
+        calculateShortestPaths();
+        setPathTo(target);
+        setSelectedDistance(target);
 
     }
-    private void calculateShortestPaths(){
+
+    public int getSelectedDistance() {
+        return selectedDistance;
+    }
+
+    public List<Integer> getSelectedPath() {
+        return selectedPath;
+    }
+
+    public void calculateShortestPaths(){
         graph.getAdjacentNodes().forEach(node-> node.forEach(d-> {
             if (distance[d.getDestination()]>d.getWeight() && distance[d.getDestination()]>d.getWeight()+ distance[d.getSource()])
             {
@@ -49,26 +68,23 @@ public class ShortestPath {
 
 
     }
-    public int[] findShortestFromTo(int source, int target) {
+    protected int[] findShortestDistances() {
         calculateShortestPaths();
-    dataPresentation();
+
         return distance;
     }
-    private void dataPresentation(){
-        //System.out.println(paths.get(2));
-        paths.forEach((k,v)-> System.out.println(paths));
-        System.out.println(paths);
-        System.out.println(Arrays.toString(distance));
+    public void setPathTo(int target){
+
+       selectedPath=paths.entrySet().stream().filter(m->m.getKey()==target).map(Map.Entry::getValue).flatMap(Collection::parallelStream).collect(Collectors.toList());
+
+        while(!selectedPath.contains(0)){
+            List<Integer> finalTarget = selectedPath;
+            selectedPath.addAll(paths.entrySet().stream().filter(m-> m.getKey().equals(finalTarget.get(0))).map(Map.Entry::getValue).flatMap(Collection::parallelStream).collect(Collectors.toList()));
+            selectedPath=selectedPath.stream().sorted().collect(Collectors.toList());
+        }
     }
-/**  shortest 3->16 [0,2,3] 2->6[0,2] 1->3[0,1]
- *        1
- *      /  |
- *     3   |
- *   /    100
- * 0       |
- *   \     |
- *     6   |
- *      \  |
- *        2----10----3
- */
+    public void setSelectedDistance(int target){
+        selectedDistance=distance[target];
+    }
+
 }
--- src/test/java/hodei/naiz/graphqllove/helpers/ShortestPathTest.java
+++ src/test/java/hodei/naiz/graphqllove/helpers/ShortestPathTest.java
@@ -14,6 +14,50 @@ import static org.junit.jupiter.api.Assertions.*;
  * Copyright: MIT
  */
 class ShortestPathTest {
+    @Test
+    void getShortestPath() {
+        Graph g=new Graph(6);
+        g.addNode(new Vertix(0,2,6));
+        g.addNode(new Vertix(0,1,3));
+
+        g.addNode(new Vertix(1,2,1));
+        g.addNode(new Vertix(2,3,10));
+        g.addNode(new Vertix(3,4,3));
+        g.addNode(new Vertix(1,4,2));
+        g.addNode(new Vertix(3,5,2));
+        ShortestPath st=new ShortestPath(g,1);
+
+        assertEquals(Arrays.asList(0),st.getSelectedPath());
+        assertEquals(3,st.getSelectedDistance());
+        ShortestPath st2=new ShortestPath(g,2);
+
+        assertEquals(Arrays.asList(0,1),st2.getSelectedPath());
+        assertEquals(4,st2.getSelectedDistance());
+        ShortestPath st3=new ShortestPath(g,3);
+
+        assertEquals(Arrays.asList(0,1,2),st3.getSelectedPath());
+        assertEquals(14,st3.getSelectedDistance());
+        ShortestPath st4=new ShortestPath(g,4);
+
+        assertEquals(Arrays.asList(0,1),st4.getSelectedPath());
+        assertEquals(5,st4.getSelectedDistance());
+        ShortestPath st5=new ShortestPath(g,5);
+
+        assertEquals(Arrays.asList(0,1,2,3),st5.getSelectedPath());
+        assertEquals(16,st5.getSelectedDistance());
+    }
+
+    /**  shortest 3->14 [0,1,2,3] 2->4[0,1,2] 1->3[0,1], 4->5[0,1,4], 5->16[0,1,2,3
+     *         [1]--2--[4]
+     *      /  |        /\
+     *     3   |        |
+     *   /    1         |
+     * [0]     |        3
+     *   \     |        |
+     *     6   |        |
+     *      \  |        |
+     *        [2]--10--[3]----2---[5]
+     */
 
     @Test
     void findShortestFromTo() {
@@ -23,21 +67,10 @@ class ShortestPathTest {
         g.addNode(new Vertix(1,2,100));
         g.addNode(new Vertix(2,3,10));
         ShortestPath st=new ShortestPath(g);
-      /*  System.out.println(Arrays.toString(st.findShortestFromTo(0, 99)));
-        System.out.println(g.getAdjacentNodes().get(0));*/
-        assertEquals(Arrays.toString(new int[]{0,3,6,16}),Arrays.toString(st.findShortestFromTo(0, 99)));
+
+        assertEquals(Arrays.toString(new int[]{0,3,6,16}),Arrays.toString(st.findShortestDistances()));
     }
-    /**  shortest 3->16 [0,2,3] 2->6[0,2] 1->3[0,1]
-     *        1
-     *      /  |
-     *     3   |
-     *   /    100
-     * 0       |
-     *   \     |
-     *     6   |
-     *      \  |
-     *        2----10----3
-     */
+
     @Test
     void findShortestFromTo2() {
         Graph g=new Graph(4);
@@ -47,22 +80,10 @@ class ShortestPathTest {
         g.addNode(new Vertix(1,2,1));
         g.addNode(new Vertix(2,3,10));
         ShortestPath st=new ShortestPath(g);
-      /*  System.out.println(Arrays.toString(st.findShortestFromTo(0, 99)));
-        System.out.println(g.getAdjacentNodes().get(0));*/
-        assertEquals(Arrays.toString(new int[]{0,3,4,14}),Arrays.toString(st.findShortestFromTo(0, 99)));
+        assertEquals(Arrays.toString(new int[]{0,3,4,14}),Arrays.toString(st.findShortestDistances()));
     }
 
-/**  shortest 3->16 [0,1,2,3] 2->4[0,1,2] 1->3[0,1]
- *        1
- *      /  |
- *     3   |
- *   /    1
- * 0       |
- *   \     |
- *     6   |
- *      \  |
- *        2----10----3
- */
+
 @Test
 void findShortestFromTo3() {
     Graph g=new Graph(5);
@@ -75,20 +96,10 @@ void findShortestFromTo3() {
     g.addNode(new Vertix(1,4,2));
     ShortestPath st=new ShortestPath(g);
 
-    assertEquals(Arrays.toString(new int[]{0,3,4,14,5}),Arrays.toString(st.findShortestFromTo(0, 99)));
+    assertEquals(Arrays.toString(new int[]{0,3,4,14,5}),Arrays.toString(st.findShortestDistances()));
 }
 
-/**  shortest 3->14 [0,1,2,3] 2->4[0,1,2] 1->3[0,1], 4->5[0,1,4]
- *         [1]--2--[4]
- *      /  |        |
- *     3   |        |
- *   /    1         |
- * [0]     |        3
- *   \     |        |
- *     6   |        |
- *      \  |        |
- *        [2]--10--[3]
- */
+
 @Test
 void findShortestFromTo4() {
     Graph g=new Graph(6);
@@ -102,7 +113,7 @@ void findShortestFromTo4() {
     g.addNode(new Vertix(3,5,2));
     ShortestPath st=new ShortestPath(g);
 
-    assertEquals(Arrays.toString(new int[]{0,3,4,14,5,16}),Arrays.toString(st.findShortestFromTo(0, 99)));
+    assertEquals(Arrays.toString(new int[]{0,3,4,14,5,16}),Arrays.toString(st.findShortestDistances()));
 }
 }
 /**  shortest 3->14 [0,1,2,3] 2->4[0,1,2] 1->3[0,1], 4->5[0,1,4], 5->10[0,1,4
